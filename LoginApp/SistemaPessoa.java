1e65e18ae858797c6ad6386b1313cc156978cd7d
Miguel Luz <migoluz@gmail.com>
2024-10-23 09:08:35 -0300

    Avaliação 3 Parcial - GUI Cadastro de Usuários
    Avaliação 3 Parcial - GUI Cadastro de Usuários

--- LoginApp/SistemaPessoa.java
+++ LoginApp/SistemaPessoa.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SistemaPessoa {
 
@@ -20,7 +21,7 @@ public class SistemaPessoa {
         JMenuItem usuariosItem = new JMenuItem("Usuários");
         JMenuItem pessoasItem = new JMenuItem("Pessoas");
 
-        usuariosItem.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Cadastrar Usuário"));
+        usuariosItem.addActionListener(e -> abrirCadastroUsuarios(frame));
         pessoasItem.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Cadastrar Pessoa"));
 
         menuCadastro.add(usuariosItem);
@@ -45,7 +46,48 @@ public class SistemaPessoa {
         frame.setVisible(true);
     }
 
+    private void abrirCadastroUsuarios(JFrame parentFrame) {
+        JFrame cadastroFrame = new JFrame("Cadastro de Usuários");
+        cadastroFrame.setSize(300, 200);
+        cadastroFrame.setLayout(new GridLayout(5, 2));
+
+        cadastroFrame.add(new JLabel("Nome:"));
+        JTextField nomeField = new JTextField();
+        cadastroFrame.add(nomeField);
+
+        cadastroFrame.add(new JLabel("Email:"));
+        JTextField emailField = new JTextField();
+        cadastroFrame.add(emailField);
+
+        cadastroFrame.add(new JLabel("Telefone:"));
+        JTextField telefoneField = new JTextField();
+        cadastroFrame.add(telefoneField);
+
+        JButton btnCadastrar = new JButton("Cadastrar");
+        JButton btnLimpar = new JButton("Limpar");
+        JButton btnSair = new JButton("Sair");
+
+        cadastroFrame.add(btnCadastrar);
+        cadastroFrame.add(btnLimpar);
+        cadastroFrame.add(btnSair);
+
+        btnCadastrar.addActionListener(e -> {
+            JOptionPane.showMessageDialog(cadastroFrame, "Botão clicado! Cadastrar");
+        });
+
+        btnLimpar.addActionListener(e -> {
+            nomeField.setText("");
+            emailField.setText("");
+            telefoneField.setText("");
+            JOptionPane.showMessageDialog(cadastroFrame, "Botão clicado! Limpar");
+        });
+
+        btnSair.addActionListener(e -> cadastroFrame.dispose());
+
+        cadastroFrame.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new SistemaPessoa());
+        SwingUtilities.invokeLater(SistemaPessoa::new);
     }
 }
